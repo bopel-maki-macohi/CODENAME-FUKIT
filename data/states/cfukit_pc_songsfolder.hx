bccc74fe5c94f122e9193b28cb9f6ce6082d6ee6
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-04 20:28:12 -0500

    songs folder scene prototype basicaly

---
+++ data/states/cfukit_pc_songsfolder.hx
@@ -0,0 +1,32 @@
+import flixel.util.FlxGradient;
+
+function create() {
+	var gradientLinear:FlxSprite = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height, [0xFFFFFFFF, 0xFFCEDEEB]);
+	gradientLinear.alpha = 0.6;
+	add(gradientLinear);
+
+	// temp until I have music in
+	FlxG.sound.music?.stop();
+}
+
+var canSelectStuff:Bool = true;
+
+function leaving(leaveScript:Void->Void) {
+	canSelectStuff = false;
+	CoolUtil.playMenuSFX(1);
+
+	FlxTween.tween(FlxG.camera, {zoom: 1.1, alpha: .75}, .75, {ease: FlxEase.sineOut});
+
+	new FlxTimer().start(0.75, function(timer) {
+		if (leaveScript != null)
+			leaveScript();
+	});
+}
+
+function update(elapsed:Float) {
+	if (controls.BACK) {
+		leaving(function() {
+			FlxG.switchState(new ModState('cfukit_pc'));
+		});
+	}
+}
