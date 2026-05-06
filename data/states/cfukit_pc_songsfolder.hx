1346a0212f01b207fb0689cf9a9c5d1036f1f081
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-05 20:24:47 -0500

    adjust star bop

--- data/states/cfukit_pc_songsfolder.hx
+++ data/states/cfukit_pc_songsfolder.hx
@@ -103,7 +103,7 @@ function parseDiff(starDiff:Int = 0) {
 
 		if (prevStarStates[star.ID] != spr) {
 			star.colorTransform.blueMultiplier = 5;
-			star.y -= star.height * FlxG.random.float(0.1, 1);
+			star.y -= star.height * Math.abs(starDiff - (star.ID + 0)) * .25;
 		}
 
 		prevStarStates[star.ID] = spr;
