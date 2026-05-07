6b04010eb762272033761626bde69707acfd4507
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-07 08:54:02 -0500

    PC Menu cleanup!

--- data/states/cfukit_pc.hx
+++ data/states/cfukit_pc.hx
@@ -6,56 +6,87 @@ import funkin.menus.ModSwitchMenu;
 import funkin.options.OptionsMenu;
 import funkin.menus.credits.CreditsMain;
 import FukitUtil;
+import PCApp;
 
 var daText:FunkinText;
-var settings:FunkinSprite;
-var songsFolder:FunkinSprite;
-var creditsTextFile:FunkinSprite;
+var settings:PCApp;
+var songsFolder:PCApp;
+var creditsTextFile:PCApp;
 
 function create()
 {
 	FukitUtil.playMenuMusic();
 
+	makeVersionText();
+
+	makeGradient();
+
+	makeApps();
+
+	FlxG.mouse.visible = true;
+}
+
+function makeVersionText()
+{
 	daText = new FunkinText(0, 16, FlxG.width, 'CODENAME FUKIT ${FukitUtil.MOD_VERSION}', 32, false);
 	daText.alignment = 'center';
 	daText.color = 0xFFFFFF;
 	daText.x = FlxG.width - daText.width;
 	add(daText);
+}
 
+function makeGradient()
+{
 	var gradientLinear:FlxSprite = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height, [0x7F4E1E61, 0xFF0D4A50]);
 	gradientLinear.alpha = 0.6;
 	add(gradientLinear);
+}
 
-	settings = new FunkinSprite(1060, 560).loadSprite(Paths.image('pc/settingsGear'));
-
-	settings.screenCenter();
+function makeApps()
+{
+	settings = new PCApp('settings');
 	settings.x -= settings.width * 2;
-
-	settings.alpha = 0.3;
 	add(settings);
 
-	songsFolder = new FunkinSprite(1060, 560).loadSprite(Paths.image('pc/songsFolder'));
+	settings.selectionFunction = function()
+	{
+		leaving(function()
+		{
+			if (!FukitUtil.getSaveField('fukit_menuMusic'))
+			{
+				FlxG.sound.playMusic(Paths.music('Fukit'));
+				FlxG.sound.music.volume = 0;
+			}
 
-	songsFolder.screenCenter();
+			FlxG.switchState(new OptionsMenu());
+		});
+	};
+
+	songsFolder = new PCApp('songsFolder');
 	songsFolder.x += songsFolder.width * 1.25;
-
-	songsFolder.alpha = 0.3;
 	add(songsFolder);
 
-	creditsTextFile = new FunkinSprite(1060, 560).loadSprite(Paths.image('pc/credits'));
+	songsFolder.selectionFunction = function()
+	{
+		leaving(function()
+		{
+			FlxG.switchState(new ModState('cfukit_pc_songsfolder', {file: 'volume1'}));
+		});
+	};
 
-	creditsTextFile.screenCenter();
-
-	creditsTextFile.alpha = 0.3;
+	creditsTextFile = new PCApp('creditsTextFile');
 	add(creditsTextFile);
 
-	FlxG.mouse.visible = true;
+	creditsTextFile.selectionFunction = function()
+	{
+		leaving(function()
+		{
+			FlxG.switchState(new CreditsMain());
+		});
+	};
 }
 
 var canSelectStuff:Bool = true;
-var hoveringOptions:Bool = false;
-var hoveringSongsFolder:Bool = false;
-var hoveringCreditsTextFile:Bool = false;
 
 function leaving(leaveScript:Void->Void)
 {
@@ -72,77 +103,9 @@ function leaving(leaveScript:Void->Void)
 
 function update(elapsed:Float)
 {
-	var overlapsSettings:Bool = FlxG.mouse.overlaps(settings);
-	settings.alpha = CoolUtil.fpsLerp(settings.alpha, overlapsSettings ? 1 : 0.3, 0.1);
-
-	if (overlapsSettings)
-	{
-		if (!hoveringOptions && canSelectStuff)
-		{
-			CoolUtil.playMenuSFX(0);
-			hoveringOptions = true;
-		}
-
-		if (FlxG.mouse.justReleased && canSelectStuff)
-		{
-			leaving(function()
-			{
-				if (!FukitUtil.getSaveField('fukit_menuMusic'))
-				{
-					FlxG.sound.playMusic(Paths.music('Fukit'));
-					FlxG.sound.music.volume = 0;
-				}
-
-				FlxG.switchState(new OptionsMenu());
-			});
-		}
-	}
-	else
-		hoveringOptions = false;
-
-	var overlapssongsFolder:Bool = FlxG.mouse.overlaps(songsFolder);
-	songsFolder.alpha = CoolUtil.fpsLerp(songsFolder.alpha, overlapssongsFolder ? 1 : 0.3, 0.1);
-
-	if (overlapssongsFolder)
-	{
-		if (!hoveringSongsFolder && canSelectStuff)
-		{
-			CoolUtil.playMenuSFX(0);
-			hoveringSongsFolder = true;
-		}
-
-		if (FlxG.mouse.justReleased && canSelectStuff)
-		{
-			leaving(function()
-			{
-				FlxG.switchState(new ModState('cfukit_pc_songsfolder', {file: 'volume1'}));
-			});
-		}
-	}
-	else
-		hoveringSongsFolder = false;
-
-	var overlappingCreds:Bool = FlxG.mouse.overlaps(creditsTextFile);
-	creditsTextFile.alpha = CoolUtil.fpsLerp(creditsTextFile.alpha, overlappingCreds ? 1 : 0.3, 0.1);
-
-	if (overlappingCreds)
-	{
-		if (!hoveringCreditsTextFile && canSelectStuff)
-		{
-			CoolUtil.playMenuSFX(0);
-			hoveringCreditsTextFile = true;
-		}
-
-		if (FlxG.mouse.justReleased && canSelectStuff)
-		{
-			leaving(function()
-			{
-				FlxG.switchState(new CreditsMain());
-			});
-		}
-	}
-	else
-		hoveringCreditsTextFile = false;
+	settings.selectionCondition = canSelectStuff;
+	songsFolder.selectionCondition = canSelectStuff;
+	creditsTextFile.selectionCondition = canSelectStuff;
 
 	if (controls.DEV_ACCESS)
 	{
---
+++ source/PCApp.hx
@@ -0,0 +1,44 @@
+package source;
+
+class PCApp extends FunkinSprite
+{
+	public var hovered:Bool = false;
+	public var overlapped:Bool = false;
+
+	public var selectionCondition:Bool = true;
+    public var selectionFunction:Dynamic = null;
+
+	public function new(app:String)
+	{
+		super(1060, 560);
+
+		loadSprite(Paths.image('pc/apps/$app'));
+
+		screenCenter();
+		alpha = 0.3;
+	}
+
+	public function update(elapsed:Float)
+	{
+		overlapped = FlxG.mouse.overlaps(this);
+		alpha = CoolUtil.fpsLerp(alpha, overlapped ? 1 : 0.3, 0.1);
+
+		if (overlapped)
+		{
+			if (!hovered && selectionCondition)
+			{
+				hovered = true;
+				CoolUtil.playMenuSFX(0);
+			}
+
+			if (FlxG.mouse.justReleased && selectionCondition)
+			{
+				if (selectionFunction != null) selectionFunction();
+			}
+		}
+		else
+		{
+			hovered = false;
+		}
+	}
+}
