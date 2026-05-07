92945bef03e38cde186a3bb5baee7ad3ab8e9598
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-07 08:55:38 -0500

    clean up options menu script

--- data/states/OptionsMenu.hx
+++ data/states/OptionsMenu.hx
@@ -15,16 +15,5 @@ function postCreate()
 function postUpdate()
 {
 	if (controls.ACCEPT)
-	{
-		for (i => thing in tree)
-		{
-			var thingName = Reflect.field(thing, 'name');
-
-			// trace('$i / $thingName : ${Reflect.field(thing, 'name')}');
-
-			// if (thingName == 'Gameplay')
-			// thing.__metronome = FlxG.sound.load(Paths.music('Fukit'));
-			FlxG.sound.music.stop();
-		}
-	}
+		FlxG.sound.music.stop();
 }
